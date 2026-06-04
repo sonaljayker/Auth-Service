3f8ce7d4558ac37360ad8cfaa6d320d1cb51b8ca
sonal kumar <sonaljayker567@gmail.com>
2026-06-04 13:51:39 +0530

    commit

--- src/main/java/com/project/authservice/security/JwtAuthenticationFilter.java
+++ src/main/java/com/project/authservice/security/JwtAuthenticationFilter.java
@@ -1,9 +1,13 @@
 package com.project.authservice.security;
 
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.MalformedJwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -11,8 +15,29 @@ import java.io.IOException;
 
 @Configuration
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private JwtService jwtService;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String header = request.getHeader("Authorization");
+        if (header != null && header.startsWith("Bearer ")) {
+            String token = header.substring(7);
+            // token extract and validate then authentication create and security context
+            try{
 
+            } catch (ExpiredJwtException e) {
+                e.printStackTrace();
+            } catch (JwtException e) {
+                e.printStackTrace();
+            } catch (MalformedJwtException e) {
+                e.printStackTrace();
+            } catch (Exception e){
+                return;
+            }
+
+        }
+        filterChain.doFilter(request, response);
     }
 }
